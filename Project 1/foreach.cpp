8e6c8fa9d60741a9b06f73d0894b7894aac0764d
Arifulla Shaik <arifullashaik01@gmail.com>
2021-09-13 13:44:11 -0700

    got -n case for foreach to work

---
+++ Project 1/foreach.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <unistd.h>
+#include <sys/wait.h>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+  //https://stackoverflow.com/questions/52467531/using-getopt-in-c-to-handle-arguments
+    switch(getopt(argc, argv, "pn:")){
+      case 'p':{
+        // todo
+      }
+    case 'n':{
+      for(int j=3; j<argc; j++){
+        for(int i=0; i<stoi(optarg); i++){
+              int pid = fork();
+              if(pid == 0){
+                cout << argv[j] << endl;
+                return NULL;
+              }else{
+                wait(&pid);
+              }
+            }
+          }
+        break;
+    }
+    default :
+        cout << "Usage: ./foreach [-p] [-n NUM] [ARGS]... PROG" << endl;
+        break;
+    }
+}
