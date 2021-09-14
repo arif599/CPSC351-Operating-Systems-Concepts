72ba214417894ed42278890d9ff289a14171adc0
Arifulla Shaik <arifullashaik01@gmail.com>
2021-09-13 23:01:18 -0700

    made echo command to work

--- Project 1/thrice.cpp
+++ Project 1/thrice.cpp
@@ -1,33 +1,51 @@
-#include <iostream>
-#include <sys/wait.h>
-#include <unistd.h>
-using namespace std;
-  
-// Driver code
-int main(int argc, char *argv[]){
+#include<iostream>
+#include<unistd.h>
+#include<string>
+#include<stdlib.h>
+#include<sys/wait.h>
+#include<vector>
+#include <cstring>
+#include <cassert>
+#include<cstdlib>
+#include<cstdio>
+#include<fcntl.h>
 
+using namespace std;
+
+
+int main(int argc, char * argv[]){
 	// if no arguments were passed
 	if(argc == 1){
 		cerr << "Usage: ./thrice PROG [ARGS]..." << endl;
 		exit(EXIT_FAILURE);
 	}
 
-	for(int i = 0; i < 3; i++){
-		// fork a child process
-      	int pid = fork();
-
-		// if process is child
-		if(pid == 0){
-			if(argv[1] == "echo"){
-			cout << argv[2] << endl;
-			}else{
-			cout << argv[1] << endl;
-			}
-			// to exit the child process
-			return NULL;
-		}else{
-			// wait for the child process to finish
-			wait(&pid);
+	//arguments to execute 
+	vector<char *> args;
+	if(strcmp(argv[1], "echo") == 0){
+		args.push_back(strdup(argv[1]));
+		for(int i=2; i<argc; i++){
+			args.push_back(strdup(argv[i]));
 		}
-    }
+	}else{
+		args.push_back(strdup(argv[1]));
+		args.push_back(NULL);
+	}
+
+	//Create 3 new child processes 
+	auto pid = fork();
+	auto pid_2 = fork();
+	if(pid < 0){
+		exit(EXIT_FAILURE);
+	}else if(pid == 0){ 
+		execvp(args[0], args.data()); 
+	}else if(pid_2 == 0){
+		execvp(args[0], args.data());
+	}else{ 
+		//Parent waits for child process to finish
+		int wc = wait(NULL);
+	}
+
+	return EXIT_SUCCESS;
+
 }
