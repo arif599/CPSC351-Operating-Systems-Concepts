f29bdf39ab939b78029066fdf6817d930d2e8af8
angyb00 <89486108+angyb00@users.noreply.github.com>
2021-09-13 23:09:05 -0700

    Update thrice.cpp

--- Project 1/thrice.cpp
+++ Project 1/thrice.cpp
@@ -1,48 +1,57 @@
-#include<iostream>
-#include<unistd.h>
-#include<string>
-#include<stdlib.h>
-#include<sys/wait.h>
-#include<vector>
-#include <cstring>
 #include <cassert>
-#include<cstdlib>
-#include<cstdio>
-#include<fcntl.h>
-
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <fcntl.h>
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <vector>
+using namespace std;
 using std::vector;
 
+int main(int argc, char *argv[]) {
 
-int main(int argc, char * argv[]){
-
-//arguments to execute 
-vector<char *> args;
-args.push_back(strdup(argv[1]));
-args.push_back(NULL);
-
-//Create 3 new child processes 
-auto pid = fork();
-auto pid_2 = fork();
-
-if(pid < 0){
-    //cout << "fork failed\n";
+  // arguments to execute
+  if (argc == 1) {
+    cerr << "Usage: ./thrice PROG [ARGS]..." << endl;
     exit(EXIT_FAILURE);
-    
-}  else if(pid == 0){
-        
-        execvp(args[0], args.data()); 
+  }
 
-}  else if(pid_2 == 0){
-    
-        execvp(args[0], args.data());
+  // arguments to execute
+  vector<char *> args;
+  if (strcmp(argv[1], "echo") == 0) {
+    args.push_back(strdup(argv[1]));
+    for (int i = 2; i < argc; i++) {
+      args.push_back(strdup(argv[i]));
+    }
+  } else {
+    args.push_back(strdup(argv[1]));
+    args.push_back(NULL);
+  }
 
-}  else { //Parent waits for child process to finish
+  // Create 3 new child processes
+  pid_t pid = fork();
+  pid_t pid_2 = fork();
+
+  if (pid < 0) {
+    // cout << "fork failed\n";
+    exit(EXIT_FAILURE);
+
+  } else if (pid == 0) {
+
+    execvp(args[0], args.data());
+
+  } else if (pid_2 == 0) {
+
+    execvp(args[0], args.data());
+
+  } else { // Parent waits for child process to finish
 
     int wc = wait(NULL);
-    
-    }
+  }
 
-return EXIT_SUCCESS;
-
+  return EXIT_SUCCESS;
 }
-
