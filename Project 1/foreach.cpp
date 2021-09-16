1c98b12ee1fec3698bea0aa41f9cd75ad68f33d9
Arifulla Shaik <arifullashaik01@gmail.com>
2021-09-15 21:58:46 -0700

    updated foreach.cpp

--- Project 1/foreach.cpp
+++ Project 1/foreach.cpp
@@ -1,11 +1,19 @@
+#include <cassert>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <fcntl.h>
 #include <iostream>
-#include <unistd.h>
+#include <stdlib.h>
+#include <string>
 #include <sys/wait.h>
+#include <unistd.h>
+#include <vector>
 using namespace std;
 
 int main(int argc, char *argv[]) {
-  //https://stackoverflow.com/questions/52467531/using-getopt-in-c-to-handle-arguments
-    switch(getopt(argc, argv, "pn:")){
+
+   switch(getopt(argc, argv, "pn:")){
       case 'p':{
         // todo
       }
@@ -25,7 +33,44 @@ int main(int argc, char *argv[]) {
         break;
     }
     default :
+      if(argc == 1){
         cout << "Usage: ./foreach [-p] [-n NUM] [ARGS]... PROG" << endl;
+
+      }else{
+      //cout << argv[argc-1] << endl;
+        //arguments to execute 
+        vector<char *> args;
+        cout << "Program: " << argv[argc-1] << endl;
+        args.push_back(strdup(argv[argc-1]));
+        for(int i=1; i<argc-1; i++){
+          cout << "arguments: " << argv[i] << endl;
+          args.push_back(strdup(argv[i]));
+        }
+
+        auto pid = fork();
+
+        if(pid < 0){
+            //cout << "fork failed\n";
+            exit(EXIT_FAILURE);
+            
+        }  else if(pid == 0){
+              execvp(args[0], args.data()); 
+              // for(int i=0; i < args.size()-1; i++){
+              //   execvp(args[i+1], args.data()); 
+              // }
+                
+
+        }else { //Parent waits for child process to finish
+
+            int wc = wait(NULL);
+            
+            }
         break;
+      }
     }
+
+
+
+return EXIT_SUCCESS;
+
 }
