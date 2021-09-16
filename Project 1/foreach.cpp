b8747137d03debdc99581ce68fb83e6dfd4883ba
angyb00 <89486108+angyb00@users.noreply.github.com>
2021-09-16 12:22:57 -0700

    New functions

--- Project 1/foreach.cpp
+++ Project 1/foreach.cpp
@@ -13,64 +13,82 @@ using namespace std;
 
 int main(int argc, char *argv[]) {
 
-//Store parameters and programs to run 
-vector<char *> args; 
-pid_t rc;
-pid_t parent_wait; 
 
-for(int i = 1; i<argc; i++){
+  vector<char *> args;
 
-  args.push_back(strdup(argv[i]));
-}
+   switch(getopt(argc, argv, "pn:")){
+      case 'p':
+        //arguments to execute 
+        //cout << "Program: " << argv[argc-1] << endl;
+        args.push_back(strdup(argv[argc-1]));
+        for(int i=2; i<argc-1; i++){
+          //cout << "arguments: " << argv[i] << endl;
+          args.push_back(strdup(argv[i]));
 
-
-for(int j = 1; j<=argc-2; j++ ){
-
-  rc = fork();
-
-
-  if(rc == 0){
-
-    execvp(args.back(), args.data());
-    //execlp(args.back(), args[j], NULL); //program to run is last arg
-  
-  } else{
-
-      if(rc < 0){return EXIT_FAILURE;}
-
-
-      else {parent_wait = wait(NULL);}
-    }
-}
-
-
-  //https://stackoverflow.com/questions/52467531/using-getopt-in-c-to-handle-arguments
-   /* switch(getopt(argc, argv, "pn:")){
-      case 'p':{
-        // todo
-      }
-    case 'n':{
-      // todo, might be incorect 
-      for(int j=3; j<argc; j++){
-        for(int i=0; i<stoi(optarg); i++){
-              int pid = fork();
-              if(pid == 0){
-                cout << argv[j] << endl;
-                return NULL;
-              }else{
-                wait(&pid);
-              }
-            }
+          pid_t pid = fork();
+          if(pid < 0){
+              //cout << "fork failed\n";
+              exit(EXIT_FAILURE);
+          }else if(pid == 0){
+            //cout << "Child program: " << args[0] << endl;
+            execvp(args[0], args.data()); 
+        }else{ //Parent does not wait for child process to finish
+            args.pop_back();
           }
+        
+        }
         break;
-    }
+    
+    case 'n':
+  
+        //cout << "Program: " << argv[argc-1] << endl;
+        args.push_back(strdup(argv[argc-1]));
+        for(int i=3; i<argc-1; i++){
+          for(int j=0; j<stoi(optarg); j++){
+            //cout << "arguments: " << argv[i] << endl;
+            args.push_back(strdup(argv[i]));
+
+            pid_t pid = fork();
+            if(pid < 0){
+                //cout << "fork failed\n";
+                exit(EXIT_FAILURE);
+            }else if(pid == 0){
+              //cout << "Child program: " << args[0] << endl;
+              execvp(args[0], args.data()); 
+            }else{ //Parent does not wait for child process to finish
+              int wc = wait(NULL);
+              args.pop_back();
+            }
+          }  
+        }
+        break;
     default :
-        //cout << "Usage: ./foreach [-p] [-n NUM] [ARGS]... PROG" << endl;
-        break;
-    }*/
+      if(argc == 1){
+        cout << "Usage: ./foreach [-p] [-n NUM] [ARGS]... PROG" << endl;
+      }else{
 
+        //arguments to execute 
+        //cout << "Program: " << argv[argc-1] << endl;
+        args.push_back(strdup(argv[argc-1]));
+        for(int i=1; i<argc-1; i++){
+          //cout << "arguments: " << argv[i] << endl;
+          args.push_back(strdup(argv[i]));
 
+          pid_t pid = fork();
+          if(pid < 0){
+              exit(EXIT_FAILURE);
+          }else if(pid == 0){
+              execvp(args[0], args.data()); 
+          }else { //Parent waits for child process to finish
+            int wc = wait(NULL);
+            args.pop_back();
+            }
+        }
+      }
+      break;
+    }
+   
 
-return EXIT_SUCCESS;
-
+   return EXIT_SUCCESS;
+   
 }
