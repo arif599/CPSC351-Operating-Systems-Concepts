bea029abeb2698c194e12da03605d79eb00801a9
angyb00 <89486108+angyb00@users.noreply.github.com>
2021-09-16 13:59:04 -0700

    Improved thrice

--- Project 1/thrice.cpp
+++ Project 1/thrice.cpp
@@ -14,48 +14,55 @@ using std::vector;
 
 int main(int argc, char *argv[]) {
 
-  pid_t pid;
-  pid_t c;
-  // arguments to execute
-  if (argc == 1) {
-    cerr << "Usage: ./thrice PROG [ARGS]..." << endl;
-    exit(EXIT_FAILURE);
-  }
 
-  // arguments to execute
-  vector<char *> args;
+pid_t pid;              //Will be used to create our child process 
+pid_t wc;              //Used for wait() to wait for child process 
+vector<char *> args;  //Used for to store arguments 
+
+  //If no arguments given 
+if (argc == 1) {
+  cerr << "Usage: ./thrice PROG [ARGS]..." << endl;
+  exit(EXIT_FAILURE);
+  }
+ 
 
   // Check if echo command
   if (strcmp(argv[1], "echo") == 0) {
-    args.push_back(strdup(argv[1]));
-    for (int i = 2; i < argc; i++) {
-      args.push_back(strdup(argv[i]));
+
+    args.push_back(strdup("echo"));  //Store command to exec 
+
+    for (int i = 2; i < argc; i++) { //Loop for amount of arguments given
+
+      //Store arguments 
+      args.push_back(strdup(argv[i])); 
     }
+
   } else {
-    args.push_back(strdup(argv[1]));
+
+    args.push_back(strdup(argv[1])); //Store first command to run 
     args.push_back(NULL);
   }
 
-  for (int i = 0; i <= 2; i++) {
-    // loop for our child process to execute
+ 
+
+  for (int i = 0; i <= 2; i++) {  //Loop 3 times to exec command 3 times
 
     pid = fork();
 
-    if (pid == 0) { // child process
+    if (pid == 0) {    //child process
 
+      //Exec given arguments in child process
       execvp(args[0], args.data());
 
-    } else {
-
-      if (pid < 0) {
+    } else if (pid < 0) {
         cerr << "fork failed\n";
         return EXIT_FAILURE;
 
-      } else { // Parent process waiting for child to finish
+    } else {    //Parent process waiting for child to finish
 
-        c = wait(NULL);
+        wc = wait(NULL);
       }
-    }
+  
   }
   return EXIT_SUCCESS;
 }
