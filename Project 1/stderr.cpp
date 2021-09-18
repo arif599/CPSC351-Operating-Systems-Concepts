067ac1d3dc4ec0ab0326487b2edef4432734beaf
angyb00 <89486108+angyb00@users.noreply.github.com>
2021-09-17 20:43:06 -0700

    Update stderr.cpp

--- Project 1/stderr.cpp
+++ Project 1/stderr.cpp
@@ -18,26 +18,33 @@ int main(int argc, char *argv[])
     cerr << "Usage: ./stderr FILE PROG [ARGS]...\n";
     exit(EXIT_FAILURE);
   }
+
   vector<char *> args;    //Used for storing arguments
   int pid = fork();
+
   if(pid < 0){
   cerr << "Fork failed.\n";
   exit(EXIT_FAILURE);
-}else if(pid == 0){
-    if(argc >= 3){
-      for(int i = 2; i < argc; i++){
+
+  }else if(pid == 0){ 
+    if(argc >= 3){    //Store all arguments if 2 or more given 
+
+      for(int i = 2; i < argc; i++){    //Store all arguments given after txt file
         args.push_back(strdup(argv[i]));
       }
 
-    }else{
-      args.push_back(strdup(argv[2]));
+    }else{    //if less than 2 arguments given 
+
+      args.push_back(strdup(argv[2]));  //Store the command to run 
       args.push_back(NULL);
     }
 
+    //Redirect output of the standard error stream of the argument given after txt file
     close(STDERR_FILENO);
     open(argv[1], O_CREAT | O_WRONLY | O_TRUNC, S_IRWXU);
-    execvp(args[0],args.data());
-  }else{
+    execvp(args[0],args.data());  //execute the remaining commands 
+
+  }else{    //In parent process, wait for child to finish
     int rc_wait = wait(NULL);
   }
 
